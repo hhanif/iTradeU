ecdcefca3e55d856cde0b0de1a1cbc78ddbf4fd6
Juan D. Cardozo <jcardozo@ucsc.edu>
2016-11-19 16:25:05 -0800

    stuff????

--- app/routes.js
+++ app/routes.js
@@ -51,6 +51,14 @@ app.post('/createItem',function(req,res){
     });
     });
 
+    //HOME PAGE ===================================
+    app.get('/home', isLoggedIn, function(req, res) {
+        res.render('../app/views/home.ejs', {
+            user : req.user,
+            item : req.item
+        });
+    });
+
     // LOGOUT ==============================
     app.get('/logout', function(req, res) {
         req.logout();
@@ -70,7 +78,7 @@ app.post('/createItem',function(req,res){
 
         // process the login form
         app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
+            successRedirect : '/home', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
