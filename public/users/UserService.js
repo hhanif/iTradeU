e16d74cc6333e583e48692849b4a021f4dd94bae
ALINA SYRTSOVA <alinasyrtsova@gmail.com>
2016-11-20 22:57:04 -0800

    added public

---
+++ public/users/UserService.js
@@ -0,0 +1,33 @@
+(function(){
+  'use strict';
+
+  angular.module('users')
+         .service('userService', ['$q', UserService]);
+
+  /**
+   * Users DataService
+   * Uses embedded, hard-coded data model; acts asynchronously to simulate
+   * remote data service call(s).
+   *
+   * @returns {{loadAll: Function}}
+   * @constructor
+   */
+  function UserService($q){
+    var users = [
+      {
+        name: 'Haris Hanif',
+        avatar: 'svg-1',
+        content: 'I want to trade my Nintendo Wii for an Xbox One.'
+      }
+    ];
+
+    // Promise-based API
+    return {
+      loadAllUsers : function() {
+        // Simulate async nature of real remote calls
+        return $q.when(users);
+      }
+    };
+  }
+
+})();
